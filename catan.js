de92be41ebedb9df2d97bf25c7508ed8a626cf7e
NicholasO68 <126284046+NicholasO68@users.noreply.github.com>
2023-04-25 14:52:19 -0400

    Update catan.js
    Added the dice rolling function

--- catan.js
+++ catan.js
@@ -1,8 +1,12 @@
-<!DOCTYPE html>
-<html>
-<body>
+function diceRoll(){
+	return Math.ceil(Math.random()*6)
+}
 
-
-
-</body>
-</html> 
+function diceFunction(){
+	let D1 = diceRoll();
+	let D2 = diceRoll();
+	
+  document.getElementById('D1').src = "die"+D1+".PNG";
+  document.getElementById('D2').src = "die"+D2+".PNG";
+  let roll = D1 + D2
+}
